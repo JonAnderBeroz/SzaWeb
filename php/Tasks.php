e094f0408055548ba5da55b7dd88c0737144689e
Jon Ander <jonanberoz2000@gmail.com>
2020-12-05 20:40:22 +0100

    basic structure of the web

---
+++ php/Tasks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="eu">
+<?php include'../html/head.html'?>
+<body>
+<aside class="sidebar">
+    <nav class="nav">
+      <ul>
+        <li class="active"><a href="Tasks.php">Eginkizunak</a></li>
+        <li><a href="importantTask.php">Garrantzitsuak</a></li>
+      </ul>
+    </nav>
+  </aside>
+</body>
+</html>
